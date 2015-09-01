df8258b642210fda101ba5454be98e4185601ae2
Amy Palamountain <amy@github.com>
2015-09-01 21:43:32 +1200

    template folder

---
+++ template/script/bootstrap.cmd
@@ -0,0 +1,24 @@
+@echo off
+echo ---------------------------------------------------------------
+echo.
+echo Hello Human
+echo Let's get your electron app dependencies ready to go
+echo.
+echo ---------------------------------------------------------------
+echo.
+echo Installing app dependencies...
+echo.
+
+npm install
+
+echo Installing dev tooling dependencies...
+echo.
+
+npm install -g electron-prebuilt@0.27.1
+npm install -g node-inspector
+npm install -g grunt-cli
+
+echo.
+echo ---------------------------------------------------------------
+echo.
+echo Done!
---
+++ template/script/server.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo ---------------------------------------------------------------
+echo.
+echo Hello Human
+echo Let's see some science!
+echo.
+echo ---------------------------------------------------------------
+echo.
+
+electron .
+
+echo.
+echo ---------------------------------------------------------------
+echo.
+echo Your app is up and running!
