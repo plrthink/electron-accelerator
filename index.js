ef4e9af584d1c1d019fac7e6d17ea29309f5db68
Amy Palamountain <amy@github.com>
2015-09-08 16:10:45 +0200

    move template writing and prompting to its own module

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 var colors = require('colors');
 var argv = require('yargs').argv;
-var prompt = require('./prompt');
-var templateWriter = require('./template-writer');
+var accelerator = require('./electron-accelerator');
 
 if (argv.version || argv.v) {
    var packageFile = require('./package.json');
@@ -10,20 +9,6 @@ if (argv.version || argv.v) {
    process.exit();
 }
 
-console.log();
-console.log('---------------------------------------------------------------\n'.rainbow);
-console.log('Hello Human\n');
-console.log('I\'d like to help you get started with your electron app\n');
-console.log('First, I\'ll ask you a few questions');
-console.log('And then I\'ll create a template that suits your needs\n');
-
-prompt.promptForSetup(function(result){
-
-  templateWriter.copyTempateWithResult(result, function(){
-    console.log('Your electron app is ready to go!\n');
-    console.log('To boostrap your application run \'script\/bootstrap\'\n');
-    console.log('Be sure to check out your projects read me for more information\n');
-    console.log('Good luck!\n');
-    console.log('---------------------------------------------------------------\n'.rainbow);
-  });
+accelerator(function(code){
+  process.exit(code)
 });
