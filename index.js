9c57101b203eb385d12624b734109a622d1b8d5f
Amy Palamountain <amy@github.com>
2015-09-21 15:26:30 +0100

    default to a given directory

--- index.js
+++ index.js
@@ -2,26 +2,36 @@
 var argv = require('yargs').argv;
 var accelerator = require('./electron-accelerator');
 
-var argv = require('yargs')
-    .usage('Usage: $0 <command> [options]')
-    .command('init', 'initalise an electron project')
-    .command('version', 'gets the version of electron-accelerator')
-    .command('help', 'show help for the electron-accelerator')
-    .example('$0 init', 'initalise an electron project in the current directory')
-    .demand(1)
-    .argv;
-
-var command = argv._[0];
-
-if (command === 'version') {
-   var packageFile = require('./package.json');
-   console.log(packageFile.name + '@' + packageFile.version);
-   process.exit();
-}
+var yargs = require('yargs')
+  .usage('\nUsage: $0 <command> [options]')
+  .command('init', 'initalise an electron project in a given directory')
+  .command('version', 'display the version of electron-accelerator')
+  .command('help', 'display help for electron-accelerator')
+  .help('h')
+  .alias('h', 'help')
+  .demand(1, 'must provide a valid command'),
+  argv = yargs.argv,
+  command = argv._[0];
 
 if (command === 'init') {
-  accelerator(function(code){
-    console.log();// empty space to terminate after a new line
-    process.exit(1)
-  });
+  yargs.reset()
+    .usage('\nUsage: $0 init -d [directory]')
+    .alias('d', 'directory')
+    .demand('d')
+    .argv
+
+    accelerator(function(code){
+      console.log();// empty space to terminate after a new line
+      process.exit(1)
+    });
+
+} else if (command === 'version'){
+
+  var packageFile = require('./package.json');
+  console.log(packageFile.name + '@' + packageFile.version);
+  process.exit();
+
+}
+else {
+  yargs.showHelp();
 }
