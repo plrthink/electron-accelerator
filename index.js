80ded4a645e346476cedd872aa94445243df5d94
Amy Palamountain <amy@github.com>
2015-09-21 01:52:45 +0100

    refactor to use yargs well

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
-var colors = require('colors');
 var argv = require('yargs').argv;
 var accelerator = require('./electron-accelerator');
 
-if (argv.version || argv.v) {
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('init', 'initalise an electron project')
+    .command('version', 'gets the version of electron-accelerator')
+    .command('help', 'show help for the electron-accelerator')
+    .example('$0 init', 'initalise an electron project in the current directory')
+    .demand(1)
+    .argv;
+
+var command = argv._[0];
+
+if (command === 'version') {
    var packageFile = require('./package.json');
    console.log(packageFile.name + '@' + packageFile.version);
    process.exit();
 }
 
-accelerator(function(code){
-  console.log();// empty space to terminate after a new line
-  process.exit(code)
-});
+if (command === 'init') {
+  accelerator(function(code){
+    console.log();// empty space to terminate after a new line
+    process.exit(1)
+  });
+}
