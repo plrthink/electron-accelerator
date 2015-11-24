5ffc5239437c5e9a9210cdea5fb3222e6b3808fb
Amy Palamountain <amy@github.com>
2015-11-23 21:57:06 -0800

    words

--- setup-squirrel.js
+++ setup-squirrel.js
@@ -6,7 +6,7 @@ module.exports = function(yargs, callback){
     .usage('\nUsage: $0 setup-squirrel <command>')
     .command('windows-s3', 'sets up squirrel releasess for windows via s3')
     .command('windows-github', 'sets up squirrel releasess for windows via GitHub')
-    .demand(2, 'please specify how you would like to set up squirrel')
+    .demand(2, 'please specify which kind of squirrel you would like to set up')
     .help('h')
     .alias('h', 'help')
     .argv;
