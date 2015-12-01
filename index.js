130897b10cc4eb192e9036c6a47cbb1d998e080f
Amy Palamountain <amy@github.com>
2015-11-30 21:32:38 -0800

    index is linted

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 #!/usr/bin/env node
-var argv = require('yargs').argv
-var fs = require('fs')
-var accelerator = require('./electron-accelerator')
-var setupSquirrel = require('./setup-squirrel')
 
-var yargs = require('yargs')
-  .usage('\nUsage: $0 <command> [options]')
-  .command('init', 'initalise an electron project in a given directory')
-  .command('setup-squirrel', 'setup a project for releases with Squirrel')
-  .command('version', 'display the version of electron-accelerator')
-  argv = yargs.argv,
-  command = argv._[0]
+'use strict'
+var argv, accelerator, setupSquirrel, yargs, command, packageFile
+
+argv = require('yargs').argv
+accelerator = require('./electron-accelerator')
+setupSquirrel = require('./setup-squirrel')
+
+yargs = require('yargs')
+.usage('\nUsage: $0 <command> [options]')
+.command('init', 'initalise an electron project in a given directory')
+.command('setup-squirrel', 'setup a project for releases with Squirrel')
+.command('version', 'display the version of electron-accelerator')
+
+argv = yargs.argv
+command = argv._[0]
 
 if (command === 'init') {
-    accelerator(yargs, function(code){
-      console.log()// empty space to terminate after a new line
-      process.exit(1)
-    })
-}
-
-else if (command === 'setup-squirrel') {
-    setupSquirrel(yargs, function(code){
-      console.log()// empty space to terminate after a new line
-      process.exit(1)
-    })
-}
-
-else if (command === 'version') {
-  var packageFile = require('./package.json')
+  accelerator(yargs, function (code) {
+    console.log()// empty space to terminate after a new line
+    process.exit(1)
+  })
+} else if (command === 'setup-squirrel') {
+  setupSquirrel(yargs, function (code) {
+    console.log()// empty space to terminate after a new line
+    process.exit(1)
+  })
+} else if (command === 'version') {
+  packageFile = require('./package.json')
   console.log(packageFile.name + '@' + packageFile.version)
   process.exit()
-}
-
-else {
+} else {
   yargs.showHelp()
 }
