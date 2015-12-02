0431849368a949600a74480c65bd0a1ed80cd37f
Amy Palamountain <amy@github.com>
2015-12-01 20:47:39 -0800

    linting

--- template/accelerator/tasks/build-packages.js
+++ template/accelerator/tasks/build-packages.js
@@ -1,18 +1,18 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict'
-  
+
   var packager = require('electron-packager')
 
-  grunt.registerTask('buildPackages', 'Builds electron packages', function(buildOption) {
+  grunt.registerTask('buildPackages', 'Builds electron packages', function (buildOption) {
     var buildOptions, done, options
 
     buildOptions = grunt.option('buildOptions')
     done = this.async()
 
-    grunt.log.subhead('Building '+ buildOptions.applicationName +' for '+ buildOptions.platform +' platform(s)\n')
+    grunt.log.subhead('Building ' + buildOptions.applicationName + ' for ' + buildOptions.platform + ' platform(s)\n')
 
     options = {
-      dir:  './',
+      dir: './',
       name: buildOptions.applicationName,
       platform: buildOptions.platform,
       arch: buildOptions.arch,
@@ -21,10 +21,8 @@ module.exports = function(grunt) {
       ignore: buildOptions.ignorePackages,
       asar: true
     }
-
     packager(options, done)
-
   })
 
-  grunt.registerTask('build', [ 'clean', 'buildPackages'])
+  grunt.registerTask('build', ['clean', 'buildPackages'])
 }
